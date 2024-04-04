4f1e8b5c912667e7031d07a32f51e24d2290b899
David Molina <davidmolflo@gmail.com>
2024-04-03 21:17:49 -0500

    Clase 03 abril

--- productos/script.js
+++ productos/script.js
@@ -1,16 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("https://fakestoreapi.com/products?limit=8&sort=desc")
+    fetch("https://fakestoreapi.com/products?sort=desc")//limit=8&
       .then((response) => response.json())
       .then((json) => listar_en_pantalla(json));
   });
 
   function listar_en_pantalla(json){
+    console.log("Cargando...");
     const productos = document.getElementById("productos");
+   // let cont = 0;
     json.forEach(element => {
+   /*   if(cont >= 5){
+        return "";
+      }*/
+    /*  console.log(element.category.charAt(0).toLowerCase());
+      if(element.category.charAt(0).toLowerCase() == 'm'){*/
         const item = document.createElement("article");
         item.className = "item";
         producto(element,item);
         productos.appendChild(item);
+      //}
+       // cont++;
 });
   }
 
